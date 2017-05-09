01623e97ed3c4e9f75053b0421e12e883d3a44e3
wang-xinyu <wangxinyu_es@163.com>
2017-05-09 22:45:47 +0800

    merge solutions for same question

--- 10_Regular_Expression_Matching.cpp
+++ 10_Regular_Expression_Matching.cpp
@@ -36,3 +36,17 @@ public:
     }
 };
 
+/**
+ * Recursion
+ */
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if (p.empty()) return s.empty();
+        if (p[1] != '*') {
+            return (s.size() > 0 && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p.substr(1)));
+        } else {
+            return (isMatch(s, p.substr(2)) || s.size() > 0 && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p));
+        }
+    }
+};
--- 23_Merge_k_Sorted_Lists.cpp
+++ 23_Merge_k_Sorted_Lists.cpp
@@ -36,3 +36,39 @@ public:
     }
 };
 
+/**
+ * Merge k Sorted Lists.
+ * Use priority queue.
+ * Time complexity: O(n*log(k))
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    struct cmp{
+        bool operator()(ListNode* a,ListNode* b) {
+            return a->val > b->val;
+        }
+    };
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, cmp> queue;
+
+        for (auto headnode : lists) {  // O(k*log(k))
+            if(headnode)  queue.push(headnode);
+        }
+
+        ListNode *head = new ListNode(0);
+        ListNode *tail = head;
+        while (!queue.empty()) {  // O(n*log(k))
+            tail->next = queue.top();
+            queue.pop();
+            tail = tail->next;
+            if (tail->next) queue.push(tail->next);  // O(log(k))
+        }
+        return head->next;
+    }
+};
